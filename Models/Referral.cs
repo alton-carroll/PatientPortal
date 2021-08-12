ac424ea6df2c133e9fc439f1079729ef6b37f4e2
Gregory Norwood <gmajorincome@gmail.com>
2021-08-12 17:53:26 -0400

    Added Referral's page

---
+++ Models/Referral.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using PatientPortal.Models;
+
+namespace PatientPortal.Models
+{
+    public class Referral
+    {
+        [Key]
+        public int ReferralId { get; set; }
+        public DateTime ReferralDate { get; set; }
+        public string Doctor { get; set; }
+        public string ReferralDoctor { get; set; }
+        public string Procedure { get; set; }
+        public string Location { get; set; }
+        public string OpenedBy { get; set; }
+        public DateTime OpenedOn { get; set; }
+        public string EditedBy { get; set; }
+        public DateTime EditedOn { get; set; }
+       
+        public Patient Patient { get; set; }
+        public int PatientId { get; set; }
+    }
+}
